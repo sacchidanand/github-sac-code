e97aa9f366f6e1f9183684cf2ddd432e59050b97
sacchidanand <sacchidanand.b@gmail.com>
2016-07-19 12:48:14 +0530

    atoi comments added

--- Basic_Code/atoi.c
+++ Basic_Code/atoi.c
@@ -1,17 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 #define MAXLINE 100
+
 /*atoi() convert String s to an integer version-1 */
-main(){
+main()
+{
   int n = 0, temp =0;
   int c=0, i=0, j=0, k=0, value =0;
   char s[MAXLINE];
   printf("\n\nEnter number : ");
 
+  //accept input as string
   while((c =getchar())!='\n'){
         s[i++] = c;
   }
   s[i]='\0';
 
-  for(i=0;isdigit(s[i]);i++){
+  //convert string to integer
+  for(i=0; isdigit(s[i]); i++){
     value = value*10 + (s[i]-'0');
   }
 
